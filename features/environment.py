69d2f0f122b22f105045aea248926ddc052fed2a
Ruslan Z <lenser87@mail.ru>
2024-06-04 21:48:45 -0700

    Integrate your tests with a Cloud Testing Platform

--- features/environment.py
+++ features/environment.py
@@ -2,17 +2,18 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.options import Options
 from app.application import Application
 
 
-def browser_init(context):
+def browser_init(context, scenario_name):
     """
     :param context: Behave context
     """
     ###GOOGLE_CHROME###
-    driver_path = ChromeDriverManager().install()
-    service = Service(driver_path)
-    context.driver = webdriver.Chrome(service=service)
+    #driver_path = ChromeDriverManager().install()
+    #service = Service(driver_path)
+    #context.driver = webdriver.Chrome(service=service)
 
     ###FIREFOX###
     #driver_path = GeckoDriverManager().install()
@@ -29,6 +30,22 @@ def browser_init(context):
     #    service=service
     #)
 
+    ### BROWSERSTACK ###
+    # Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
+    bs_user = 'ruslanzverev_zbgv6U'
+    bs_key = 'yyhyNhcYJz7F5v2d4u88'
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+
+    options = Options()
+    bstack_options = {
+        'os': 'OS X',
+        'osVersion': 'Sonoma',
+        'browserName': 'Firefox',
+        'sessionName': 'User can filter by sale status Out of Stocks'
+    }
+    options.set_capability('bstack:options', bstack_options)
+    context.driver = webdriver.Remote(command_executor=url, options=options)
+
     context.driver.maximize_window()
 
     context.driver.maximize_window()
@@ -39,7 +56,7 @@ def browser_init(context):
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
-    browser_init(context)
+    browser_init(context, scenario.name)
 
 
 def before_step(context, step):
